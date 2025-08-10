a171263b2c097d0459ebadaed91336530d4354d2
Anognya Reddy <anognyareddyjr@gmail.com>
2025-08-10 14:06:45 +0530

    Delete AI-Lab-1 directory

--- AI-Lab-1/task-1.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(s):
-    # Remove non-alphanumeric characters and convert to lowercase
-    cleaned = ''.join(c.lower() for c in s if c.isalnum())
-    return cleaned == cleaned[::-1]
-
-# Example usage:
-input_str = "A man, a plan, a canal: Panama"
-print(is_palindrome(input_str))  # Output: True
--- AI-Lab-1/task-3.py
+++
@@ -1,8 +0,0 @@
-def reverse_string(s):
-    
-    return s[::-1]
-
-# Example usage:
-if __name__ == "__main__":
-    input_str = input()
-    print(reverse_string(input_str))  
